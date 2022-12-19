b99f05094b157d8bfb4f5eb4d03b240641900e38
Joseph Tessitore <joetgamerman3@gmail.com>
2022-12-18 19:54:22 -0500

    Got the query working. IDK whats up with it

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/PuerkitoBio/goquery"
+import (
+	"fmt"
+	"log"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 func main() {
-	doc, err := goquery.NewDocument("https://www.omniglot.com/conscripts/sitelenpona.html")
+	doc, err := goquery.NewDocument("https://www.omniglot.com/conscripts/sitelenpona.htm")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("*").Each(func(i int, s *goquery.Selection) {
+		url, exists := s.Attr("src")
+		if exists {
+			fmt.Println(url)
+		}
+	})
 }
